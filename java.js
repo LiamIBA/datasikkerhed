34b9cdad882dcb8a89ad843801e8ea3f5fcf0bd6
LiamIBA <Liajoe01@IBA.dk>
2024-05-07 14:58:56 +0200

    update

--- java.js
+++ java.js
@@ -23,7 +23,7 @@ function getPasswordStrength(password) {
         1: { label: "Meget svag", description: "Dit password er meget svagt og let at gætte. Forøg sikkerheden ved at blande store og små bogstaver, tilføje tal samt specialtegn, og gør det længere end 14 tegn.", color: "#f44336" },
         2: { label: "Svag", description: "Dit password er svagt og kunne være mere sikkert. Prøv at styrke det ved at inkludere en blanding af store bogstaver, tal og symboler for at gøre det sværere at gætte.", color: "#ff9800" },
         4: { label: "Middel", description: "Dit password er middel, men det kan forbedres. Tilføj flere komplekse tegn, såsom symboler og blandede bogstavstørrelser, og overvej at øge længden.", color: "#ffeb3b" },
-        5: { label: "Stærk", description: "Godt arbejde! Dit password er stærkt. Husk at et ideelt stærkt password indeholder mindst 14 tegn, inkluderer både store og små bogstaver samt tal og symboler..", color: "#4caf50" },
+        5: { label: "Stærk", description: "Godt arbejde! Dit password er stærkt. Husk at et ideelt stærkt password indeholder mindst 14 tegn, inkluderer både store og små bogstaver samt tal og symboler.", color: "#4caf50" },
         6: { label: "Meget stærk", description: "Fantastisk! Dit password er meget stærkt og godt sikret. Vær sikker på at det har en blanding af store og små bogstaver, tal, specialtegn, og er på mindst 14 tegn.", color: "#009688" }
     };
 
@@ -38,12 +38,10 @@ function getPasswordStrength(password) {
     if (password.match(/[^A-Za-z0-9]/)) score++;
     if (password.match(/[\.\,\?\!\@\#\$\%\^\&\*\(\)\-\=\_\+\[\]\{\}\;\:\'\"\<\>\|\\\//]/)) score++;
 
-    
     if (password.length >= 10 && password.match(/[A-Z]/) && password.match(/[a-z]/) && password.match(/[0-9]/)) {
         score = Math.max(score, 5); 
     }
 
-    
     if (password.length >= 14 && password.match(/[A-Z]/) && password.match(/[a-z]/) && password.match(/[0-9]/) && password.match(/[\.\,\?\!\@\#\$\%\^\&\*\(\)\-\=\_\+\[\]\{\}\;\:\'\"\<\>\|\\\//]/)) {
         score = Math.max(score, 6); 
     }
@@ -56,6 +54,6 @@ function checkPasswordStrength() {
     const strengthDisplay = document.getElementById('strengthDisplay');
     const strength = getPasswordStrength(password);
 
-    strengthDisplay.textContent = `Styrke: ${strength.label}`;
+    strengthDisplay.textContent = `Styrke: ${strength.label} - ${strength.description}`;
     strengthDisplay.style.backgroundColor = strength.color;
 }
